475dde9f06873db26cdd340651de50bf02e87cc9
Enrique Martín García <enriquem.developer@gmail.com>
2019-10-22 02:08:03 +0200

    initial commit - basic app scaffold

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { Reset } from "~/styles.global";
+import Dummy from "./components/Dummy";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Reset />
+      <Dummy />
+    </>
+  );
+};
+
+export default App;
