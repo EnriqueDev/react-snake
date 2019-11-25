00aa64e7dce40313cdbdb711388913898c947e04
Enrique Martin <enrique.martin-garcia@aboutyou.de>
2019-11-26 00:15:46 +0100

    refactored logic to be handled from GameManager, clear separation of concerns

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,37 +1,30 @@
 import * as React from 'react'
 import { hot } from 'react-hot-loader'
-import BoardManager from './managers/BoardManager'
-import FrameManager from './managers/FrameManager'
-import SnakeManager from './managers/SnakeManager'
 import { Position } from './managers/Snake/SnakeNode'
-import { addEventListeners } from './utils'
 import Board from './components/Board'
-
+import GameManager from './GameManager'
 import { Reset } from './styles'
 
-const boardManager = new BoardManager()
-const frameManager = new FrameManager()
-const snakeManager = new SnakeManager()
+const gameManager = new GameManager()
 
 const App: React.FC = () => {
   const [snake, setSnake] = React.useState<Position[]>([])
 
   const runFrameUpdate = React.useCallback(() => {
-    snakeManager.moveSnake()
-    const snake = snakeManager.getSnake()
+    gameManager.runSystemFrame()
+    const snake = gameManager.getState()
     setSnake(snake)
   }, [])
 
   React.useEffect(() => {
-    setSnake(snakeManager.getSnake())
-    frameManager.init(runFrameUpdate)
-    addEventListeners(snakeManager)
+    setSnake(gameManager.getState())
+    gameManager.init(runFrameUpdate)
   }, [])
 
   return (
     <>
       <Reset />
-      <Board snake={snake} cells={boardManager.getBoard()} />
+      <Board snake={snake} cells={gameManager.getBoard()} />
     </>
   )
 }
---
+++ src/app/GameManager.ts
@@ -0,0 +1,55 @@
+import BoardManager from './managers/BoardManager'
+import FrameManager from './managers/FrameManager'
+import SnakeManager from './managers/SnakeManager'
+import { Position } from './managers/Snake/SnakeNode'
+
+export default class GameManager {
+  private boardManager: BoardManager
+  private frameManager: FrameManager
+  private snakeManager: SnakeManager
+
+  constructor() {
+    this.boardManager = new BoardManager()
+    this.frameManager = new FrameManager()
+    this.snakeManager = new SnakeManager()
+  }
+
+  getState = (): Position[] => {
+    return this.snakeManager.getSnake()
+  }
+
+  getBoard = (): any[][] => {
+    return this.boardManager.getBoard()
+  }
+
+  init(callBack: () => void) {
+    this.addEventListeners()
+    this.frameManager.init(callBack)
+  }
+
+  runSystemFrame = () => {
+    this.snakeManager.moveSnake()
+  }
+
+  addEventListeners = () => {
+    addEventListener('keydown', (event: KeyboardEvent) => {
+      switch (event.which) {
+        case 80:
+          this.snakeManager.togglePause()
+          break
+        case 37:
+          this.snakeManager.setLastPressedKey('left')
+          break
+        case 38:
+          this.snakeManager.setLastPressedKey('up')
+          break
+        case 39:
+          this.snakeManager.setLastPressedKey('right')
+          break
+        case 40:
+          this.snakeManager.setLastPressedKey('down')
+          break
+      }
+    })
+  }
+}
