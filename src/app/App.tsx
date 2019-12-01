6b4594941b4b3d9750f9c1652101b0733d1116f6
Enrique Martín García <25365731+EnriqueDev@users.noreply.github.com>
2019-12-01 02:00:27 +0100

    added score counter (#12)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react'
 import { hot } from 'react-hot-loader'
-import Board from './components/Board'
 import GameManager from './GameManager'
 import { Reset } from './styles'
+import Game from './Game'
 
 const gameManager = new GameManager()
 
@@ -10,20 +10,26 @@ const App: React.FC = () => {
   const [snake, setSnake] = React.useState<Map<string, boolean>>(new Map())
   const [cells, setCells] = React.useState<any[][]>([])
   const [food, setFood] = React.useState<string>('')
+  const [score, setScore] = React.useState<number>(0)
 
   const runFrameUpdate = React.useCallback(() => {
     gameManager.runSystemFrame()
-    const { snake, foodPosition } = gameManager.getState()
+    const { snake, foodPosition, score: newScore } = gameManager.getState()
     setSnake(snake)
     if (food !== foodPosition) {
       setFood(foodPosition)
     }
+
+    if (newScore !== score) {
+      setScore(newScore)
+    }
   }, [])
 
   React.useEffect(() => {
-    const { snake, foodPosition } = gameManager.getState()
+    const { snake, foodPosition, score } = gameManager.getState()
     setSnake(snake)
     setFood(foodPosition)
+    setScore(score)
     setCells(gameManager.getCells())
     gameManager.init(runFrameUpdate)
   }, [])
@@ -31,7 +37,7 @@ const App: React.FC = () => {
   return (
     <>
       <Reset />
-      <Board foodPosition={food} snake={snake} cells={cells} />
+      <Game score={score} foodPosition={food} snake={snake} cells={cells} />
     </>
   )
 }
